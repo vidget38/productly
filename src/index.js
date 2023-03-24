09978de712e1dfa5cce344f5fbcc8d26f3c3b75e
vidget38 <vidget38@mail.ru>
2023-03-24 16:36:06 +0300

    update: add event for strategies section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-console.log("Hello, wolrd!");
+window.onload = function() {
+    console.log("Hello, world!");
+
+    //Tags
+    addTagsClickHandler();
+}
+
+const addTagsClickHandler = () => {
+    document.querySelector('.strategies__tags').addEventListener('click', (e) => {
+        if (e.target.classList.contains('tag')) {
+            let clickedTag = e.target;
+            removeSelectedTags();
+            selectClickedTags (clickedTag);
+            if(clickedTag.innerText === 'All') {
+                showAllStrategies();
+            } else {
+                filterStrategyBySelectedTag(clickedTag.innerText);
+            }
+        }
+    });
+}
+
+const removeSelectedTags = () => {
+    let tags = document.querySelectorAll('.strategies__tags .tag');
+    tags.forEach(tag => {
+        tag.classList.remove('tag_selected');
+        tag.classList.add('tag_bordered');
+    })
+}
+
+const selectClickedTags = (clickedTag) => {
+    clickedTag.classList.add('tag_selected');
+    clickedTag.classList.remove('tag_bordered');
+}
+
+const showAllStrategies = () => {
+    let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+    strategies.forEach(strategy => {
+        strategy.classList.remove('strategy_hidden');
+    });
+    
+}
+
+const filterStrategyBySelectedTag = (selectedTag) => {
+    let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+    strategies.forEach(strategy => {
+        strategy.classList.add('strategy_hidden');
+        strategy.querySelectorAll('.tag').forEach(tag => {
+            if(tag.innerText === selectedTag) {
+                strategy.classList.remove('strategy_hidden');
+            }
+        });
+    });
+}
