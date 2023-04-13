4bdbd2f03356c2f4052fa3355b5fb934d30a7160
vidget38 <vidget38@mail.ru>
2023-04-13 13:41:38 +0300

    update: add plugin and loaders in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,63 @@
 const webpack = require('webpack');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env, options) => {
     const isProduction = options.mode === 'production';
 
     const config = {
         mode: isProduction ? 'production' : 'development',
-        devtool: isProduction ? 'none' : 'hidden-source-map',
+        devtool: isProduction ? 'inline-source-map' : 'hidden-source-map',
         watch: !isProduction,
-        entry: './src/index.js',
+        entry: ['./src/index.js', '/src/sass/style.scss'],
         output: {
             path: path.join(__dirname, '/dist'),
             filename: 'script.js', 
         },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                      loader: 'babel-loader',
+                      options: {
+                        presets: [
+                          ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                      }
+                    }
+                  },
+                  {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader ,'css-loader', 'sass-loader'
+                    ]
+                  },
+                  {
+                    test: /\.(png|svg|jpe?g|gif)$/i,
+                    use: [
+                      {
+                        loader: 'file-loader',
+                      },
+                    ],
+                  },
+                  {
+                    test: /\.html$/i,
+                    loader: "html-loader",
+                  },
+            ]
+        },
         plugins: [
             new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css'
+            })
         ]
     }
 
